e8dde06072100ec48212529831b8797644b2e841
Akmal <akmalthemalong@gmail.com>
2026-02-05 07:13:20 +0800

    feat: tambahkan route untuk landing

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@ use App\Livewire\Admin\Profile;
 use App\Livewire\Admin\ComponentDocs;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
+use App\Livewire\Landing;
 use App\Http\Controllers\Admin\LogoutController;
+
+// Landing Page
+Route::get('/', Landing::class)->name('home');
 
 // Auth Routes
 Route::get('/login', Login::class)->name('login');
